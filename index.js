9c3bdd9c7212850639d78e75df21c3a40bfda091
Mohamad Tassieh <mohamad.bassam.tessieh@gmail.com>
2021-01-01 15:10:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ let newTime = time.split(":")[0];
 if (newTime < 12){
   return "Good Morning";
 }
-// else if (newTime >= 12 && time <= 17){
-//   return "Good Afternoon";
-//}
-else if (newTime > 17 && newTime <= 24) {
-  return "Good Evening";
-}
-else {
+else if (newTime >= 12 && newTime <= 17){
   return "Good Afternoon";
+}
+// else if (newTime > 17 && newTime <= 24) {
+//   return "Good Evening";
+// }
+else {
+  return "Good Evening";
 }
 }
 
